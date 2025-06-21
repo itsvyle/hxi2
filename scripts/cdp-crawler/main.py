fae3d5f18f61c616695c32d2b5f7f527b5fa460d
itsvyle <65409906+itsvyle@users.noreply.github.com>
2025-06-21 16:24:14 +0200

    fix

--- scripts/cdp-crawler/main.py
+++ scripts/cdp-crawler/main.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup
 from urllib import request as urlr
 import os
 from cookies import *
-CDPLINK = "https://cahier-de-prepa.fr/mp2i-parc"
-# à obtenir en se connectant à cdp et en regardant les cookies dans le navigateur
-# COOKIES = "CDP_SESSION_PERM=xxxxxxxx; CDP_SESSION=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 
 SECTIONS = ["phys","general","info","SI","Francais"]
